ec27984735a48cca07250f59d680a1abf25810e4
charlotte-ye <yye14@artic.edu>
2025-10-22 14:03:09 -0500

    points.js

--- points.js
+++ points.js
@@ -3,26 +3,23 @@ const noiseScale = 0.002;
 const noiseSpeed = 0.02;
 
 let col1, col2;
+
 function setup() {
   const c = createCanvas(windowWidth, windowHeight);
-
   c.style('position', 'fixed');
   c.style('top', '0');
   c.style('left', '0');
-  c.style('z-index', '-1');          
-  c.style('pointer-events', 'none');
+  c.style('z-index', '-1');          // 在文字后面
+  c.style('pointer-events', 'none'); // 不挡链接/滚动
+
   noStroke();
   col1 = color('#3BBFDA');
   col2 = color('#99FFCC');
 }
 
-
-
 function draw() {
   background(255);
-  const xd = width / res;
-  const yd = height / res;
-
+  const xd = width / res, yd = height / res;
   for (let x = 0; x < width; x += xd) {
     for (let y = 0; y < height; y += yd) {
       const n = noise(x * noiseScale, y * noiseScale, frameCount * noiseSpeed);
@@ -33,5 +30,5 @@ function draw() {
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight); 
+  resizeCanvas(windowWidth, windowHeight);
 }
