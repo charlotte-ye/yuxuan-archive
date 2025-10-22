0c5ee04993168bef34e3b81b320e1a085eb88ab2
charlotte-ye <yye14@artic.edu>
2025-10-22 13:53:25 -0500

    Update points.js

--- points.js
+++ points.js
@@ -1,27 +1,37 @@
-let paused = false; 
+const res = 40;
+const noiseScale = 0.002;
+const noiseSpeed = 0.02;
 
+let col1, col2;
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  noStroke();
+  const c = createCanvas(windowWidth, windowHeight);
 
+  c.style('position', 'fixed');
+  c.style('top', '0');
+  c.style('left', '0');
+  c.style('z-index', '-1');          
+  c.style('pointer-events', 'none');
+  noStroke();
+  col1 = color('#3BBFDA');
+  col2 = color('#99FFCC');
 }
+
+
 
 function draw() {
-  if (paused) return;   
+  background(255);
+  const xd = width / res;
+  const yd = height / res;
 
-  let r = random(150, 230);
-  let g = random(150, 230);
-  let b = random(150, 230);
-
-  fill(r, g, b, 120);
-  ellipse(random(width), random(height), 8, 8);
-}
-
-
-function mousePressed() {
-  paused = !paused;
+  for (let x = 0; x < width; x += xd) {
+    for (let y = 0; y < height; y += yd) {
+      const n = noise(x * noiseScale, y * noiseScale, frameCount * noiseSpeed);
+      fill(lerpColor(col1, col2, n));
+      rect(x, y, xd, yd);
+    }
+  }
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, windowHeight); 
 }
